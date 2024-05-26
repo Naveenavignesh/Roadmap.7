030f295a5fe5dbcde69d0e2f4305958eabc456e5
Naveenavignesh <naveenanavii4437@gmail.com>
2024-05-26 07:47:39 -0700

    Added :Documemt/array,js/script.js

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const countries =[
+    {name:'india',continent:'asia'},
+    {name:'china', continent:'asia'},
+    {name:'greece', continent:'europe'},
+
+]
+let asiancountries = countries.filter(country =>{
+    return country.continent =='asia';
+})
+console.log(asiancountries);
+const countries2= [
+    {name:'india',continent:'asia'},
+    {name:'greece',continent:'europe'},
+]
+let asiancountries2 = countries.filter(country =>country.population<200000);
+    console.log(asiancountries2);
+    const countries3= [
+        {name:'india',capital:'Delhi'},
+        {name:'paskitan',capital:'Islamabed'},
+        {name:'greece',capital:'Athens'}
+    ]
+let asiancountries3= countries.foreach(country =>{
+    console.log(country);
+    return country;
+})
+     const countries4= [
+        {name:'india',asia:'1000123'},
+        {name:'china',asia:'270123'},
+     ]
+let asiancountries4 =[countries.reduce => {return current + population;0
+    console.log(asiancountries4);
+}]
+
+console.log(asiancountries4);
+ const countries5= [
+    {name:'indian',currency:'Rs'},
+    {name:'America',currency:'Dollar'},
+    {name:'china',currency:'Yuan'},
+    {name:'greece',currency:'GRD'},
+ ]
+let asiancountries5 =(country=>{return dollarcurrency;
+
+})
+console.log(asiancountries5);
+
+    
